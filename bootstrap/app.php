00f9e307153692ddc8c4636cfb7bbb5c3b4967a8
Hazem <hazem.ismail@hotmail.com>
2026-09-06 21:09:19 +0300

    fix csrf mismatch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EnsurePermission;
 use App\Http\Middleware\EnsureUserIsActive;
+use App\Http\Middleware\ShareCsrfToken;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,6 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->appendToGroup('web', ShareCsrfToken::class);
+        $middleware->appendToGroup('api', ShareCsrfToken::class);
         $middleware->alias([
             'active' => EnsureUserIsActive::class,
             'permission' => EnsurePermission::class,
